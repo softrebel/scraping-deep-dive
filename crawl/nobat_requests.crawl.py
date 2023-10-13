6fcd2f2a1c0fbf80f6ad24b224e0729b2060809d
shokhmatic <shokhmatic@shokhmatic.os>
2023-10-13 13:50:42 +0330

    change nobat scraping

--- crawl/nobat_requests.crawl.py
+++ crawl/nobat_requests.crawl.py
@@ -1,38 +1,85 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import json
 
+import time
+
+
+start = time.time()
 os.chdir(os.path.dirname(__file__))
-base_url='https://nobat.ir/find/city-1/c-7/page-{page}'
-doctor_list=[]
-doctor_url = 'https://nobat.ir{url}'
-page=1
-while True:
-    url=base_url.format(page=page)
-    print('crawling ',url)
-    res=requests.get(url)
-    soup=BeautifulSoup(res.text,'lxml')
-    doctors=soup.find_all('a',{'class':'drList nicehover'})
+base_url = "https://nobat.ir/find/city-1/c-7/page-{page}"
+doctor_list = []
+doctor_url = "https://nobat.ir{url}"
+page = 1
+for item in range(1, 6):
+    page = item
+    url = base_url.format(page=page)
+    print("crawling ", url)
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, "lxml")
+    doctors = soup.find_all("a", {"class": "doctor-ui"})
+    # doctors = []
     for doctor in doctors[:10]:
-        doctor_dict= dict()
-        doctor_dict['name']=doctor.find('div',{'class':'mainDetail'}).find('div',{'class':'drName'}).text.strip()
-        doctor_dict['profession']=doctor.find('div',{'class':'mainDetail'}).find('div',{'class':'drSpecialty'}).find('h3').text.strip()
-        print('crawling',doctor_dict['name'])
-        res=requests.get(doctor_url.format(url=doctor['href']))
-        doc_soup=BeautifulSoup(res.text,'lxml')
-        extra = []
-        for field in doc_soup.select('li[class=list-inline-item]>span'):
-            extra.append(field.text.strip())
-        doctor_dict['extra']=extra
-        doctor_list.append(doctor_dict)
+        doctor_dict = dict()
+        doctor_dict["name"] = (
+            doctor.find("h2", {"class": "doctor-ui-name"}).find("span").text.strip()
+        )
+        doctor_dict["profession"] = []
+        specialties = doctor.find_all("span", {"class": "doctor-ui-specialty"})
+        for item in specialties:
+            doctor_dict["profession"].append(item.text.strip())
 
-    
-    last=int(soup.find_all('a',{'class':'pageButton'})[-1].text.strip())
+        # doctor_dict["profession"] = (
+        #     doctor.find("div", {"class": "doctor-ui-specialty"})
+        #     .find("div", {"class": "drSpecialty"})
+        #     .find("h3")
+        #     .text.strip()
+        # )
+        print("crawling", doctor_dict["name"])
+        res = requests.get(doctor["href"])
+        doc_soup = BeautifulSoup(res.text, "lxml")
+
+        comment_popup = doc_soup.find("a", {"data-role": "comments-popup"})
+        doctor_dict["comments"] = []
+        if comment_popup:
+            comments_url = comment_popup.attrs["href"]
+            comments_id = comments_url.split("/")[-1]
+            comment_api_url = (
+                f"https://nobat.ir/api/public/doctor/comments/all/{comments_id}/0"
+            )
+
+            res = requests.get(comment_api_url)
+            comments = json.loads(res.text)[0]
+            doctor_dict["comments"] = comments
+        doctor_list.append(doctor_dict)
+        # comments_soup = BeautifulSoup(res.text, "lxml")
+
+        # comments = []
+
+        # comments_css = doc_soup.find_all(
+        #     "div", {"class": "comments_comment_box__2F_q8"}
+        # )
+        # for comment in comments_css:
+        #     text = comment.select("div > span")
+        #     date = comment.select("small")
+        #     comments.append(dict(text=text, date=date))
+        # # for field in doc_soup.select("li[class=list-inline-item]>span"):
+        # #     extra.append(field.text.strip())
+        # doctor_dict["comments"] = comments
+        # doctor_list.append(doctor_dict)
+
+    # last_soup = soup.find_all('li',{'class':'pagination-item'})
+    # last = int(soup.find_all("a", {"class": "pagination-item"})[-1].text.strip())
     # last = int(response.xpath("//div[@class='paging']/a[last()]/text()").extract_first())
-    if page >= last:
-        break
-    page+=1
- 
-with open('output_crawl_requests.json','w',encoding='utf-8') as f:
+    # if page >= last:
+    #     break
+    # page += 1
+
+with open("output_crawl_requests.json", "w", encoding="utf-8") as f:
     import json
-    f.write(json.dumps(doctor_list,ensure_ascii=False,indent=4))
+
+    f.write(json.dumps(doctor_list, ensure_ascii=False, indent=4))
+end = time.time()
+print("time elapsed:")
+print(end - start)
--- crawl/nobat_scrapy_crawl.py
+++ crawl/nobat_scrapy_crawl.py
@@ -1,53 +1,95 @@
 import scrapy
 from scrapy.crawler import CrawlerProcess
 import os
+import time
+
+start = time.time()
 
 os.chdir(os.path.dirname(__file__))
+LAST_PAGE = 5
 
 
 class NobatDoctorSpider(scrapy.Spider):
-    name = 'nobatspider'
-    start_urls = ['https://nobat.ir/find/city-1/c-7/page-1', ]
-    url_format = 'https://nobat.ir/find/city-1/c-7/page-{page}'
-    found_doctors = []
-    doctor_url = 'https://nobat.ir{url}'
+    name = "nobatspider"
+    start_urls = [
+        "https://nobat.ir/find/city-1/c-7/page-1",
+    ]
+    url_format = "https://nobat.ir/find/city-1/c-7/page-{page}"
+    # found_doctors = []
+    doctor_url = "https://nobat.ir{url}"
 
     def parse(self, response):
-        print('url:', response.url)
-        page = int(response.url.split('page-')[-1])
-        print('test')
-        for doctors in response.xpath("//a[@class='drList nicehover']"):
-            name = doctors.xpath(
-                "div[@class='mainDetail']/div[contains(@class,'drName')]/text()").extract_first().strip()
-            profession = doctors.xpath(
-                "div[@class='mainDetail']/div[contains(@class,'drSpecialty')]/h3/text()").extract_first().strip()
-            self.found_doctors.append(dict(name=name, profession=profession))
-            item = dict(name=name, profession=profession)
-            yield scrapy.Request(self.doctor_url.format(url=doctors.attrib['href']), callback=self.parse_doctor_page, meta={'item': item})
-        last = int(response.xpath(
-            "//div[@class='paging']/a[last()]/text()").extract_first())
-        if page < last:
-            yield scrapy.Request(self.url_format.format(page=page+1), callback=self.parse)
+        print("url:", response.url)
+        if not "page-" in response.url:
+            page = 1
+        else:
+            page = int(response.url.split("page-")[-1])
+        print("test")
+        for doctors in response.xpath("//a[@class='doctor-ui']"):
+            name = doctors.xpath("div/div/h2/span/text()").extract_first().strip()
+            professions = []
+            specialties = doctors.xpath(
+                'div/div/span[@class="doctor-ui-specialty"]/text()'
+            )
+            for item in specialties:
+                professions.append(item.extract().strip())
+            # profession = (
+            #     doctors.xpath(
+            #         "div[@class='mainDetail']/div[contains(@class,'drSpecialty')]/h3/text()"
+            #     )
+            #     .extract_first()
+            #     .strip()
+            # )
+            # self.found_doctors.append(dict(name=name, professions=professions))
+            item = dict(name=name, professions=professions)
+            yield scrapy.Request(
+                doctors.attrib["href"],
+                callback=self.parse_doctor_page,
+                meta={"item": item},
+            )
+
+        if page <= LAST_PAGE:
+            yield scrapy.Request(
+                self.url_format.format(page=page + 1), callback=self.parse
+            )
 
     def parse_doctor_page(self, response):
-        print('url', response.url)
-        extra = []
-        extra_field_xpath = "//li[@class='list-inline-item']/span/text()"
-        for field in response.xpath(extra_field_xpath):
-            extra.append(field.extract().strip())
-        response.request.meta['item']['extra'] = extra
+        print("url", response.url)
 
-        return response.request.meta['item']
+        comment_popup = response.xpath("//a[@data-role='comments-popup']")
+        if not comment_popup or len(comment_popup) == 0:
+            response.request.meta["item"]["comments"] = []
+            yield response.request.meta["item"]
+
+        comments_url = comment_popup.attrib["href"]
+        comments_id = comments_url.split("/")[-1]
+        comment_api_url = (
+            f"https://nobat.ir/api/public/doctor/comments/all/{comments_id}/0"
+        )
+        yield scrapy.Request(
+            comment_api_url,
+            callback=self.parse_doctor_comment_page,
+            meta={"item": response.request.meta["item"]},
+        )
+
+    def parse_doctor_comment_page(self, response):
+        import json
+
+        comments = json.loads(response.text)[0]
+        response.request.meta["item"]["comments"] = comments
+        return response.request.meta["item"]
 
 
-if __name__ == '__main__':
-    process = CrawlerProcess({
-        'LOG_LEVEL': 'DEBUG',
-        'FEED_FORMAT': 'json',
-        'FEED_URI':'result.json',
-        'FEED_EXPORT_ENCODING':'utf-8',
-        'CONCURRENT_REQUESTS':100,
-    })
+if __name__ == "__main__":
+    process = CrawlerProcess(
+        {
+            "LOG_LEVEL": "DEBUG",
+            "FEED_FORMAT": "json",
+            "FEED_URI": "result.json",
+            "FEED_EXPORT_ENCODING": "utf-8",
+            "CONCURRENT_REQUESTS": 100,
+        }
+    )
     process.crawl(NobatDoctorSpider)
     spider = next(iter(process.crawlers)).spider
     process.start()
@@ -55,3 +97,7 @@ if __name__ == '__main__':
     # with open('output_scrapy_crawl.json', 'w', encoding='utf-8') as f:
     #     import json
     #     f.write(json.dumps(spider.found_doctors, ensure_ascii=False, indent=4))
+
+    end = time.time()
+    print("time elapsed:")
+    print(end - start)
